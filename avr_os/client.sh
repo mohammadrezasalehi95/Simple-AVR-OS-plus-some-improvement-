ec02a0157a20fc24ef800c18b0e3417aa6574942
mohammadsaelhi95 <mohammadreza.salehi951@gmail.com>
2019-05-04 12:21:56 +0430

    km

--- avr_os/client.sh
+++ avr_os/client.sh
@@ -2,7 +2,7 @@ git pull origin master
 git add .
 git commit -m $1
 git push origin master
-netcat 192.168.8.100 4444<client.sh
+timeout 5 netcat 192.168.8.100 4444<client.sh
 sleep 2s
 echo ended
 while true; do
