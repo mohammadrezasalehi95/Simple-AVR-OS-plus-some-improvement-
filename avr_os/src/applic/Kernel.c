df29e4fad6ae39c4b29f355a314c7105f3ba1084
mohammadsaelhi95 <mohammadreza.salehi951@gmail.com>
2019-05-04 12:24:38 +0430

    km

--- avr_os/src/applic/Kernel.c
+++ avr_os/src/applic/Kernel.c
@@ -62,124 +62,118 @@ volatile INT8U v_SysStat;
 volatile INT8U v_StatReg;
 
 
-int main(void)
-{
-	v_SysStat = 0;
-	INT8U v_led = 0;
-	INT8U v_key;
-	INT8U buf_g[9];
+int main(void) {
+    v_SysStat = 0;
+    INT8U v_led = 0;
+    INT8U v_key;
+    INT8U buf_g[9];
 
-	f_InitSysTimer();
+    f_InitSysTimer();
 
 #ifdef MOD_UART_ON
-	f_ConfigSerial();
+    f_ConfigSerial();
 #else
 #warning "No UART"
 #endif
 
-#ifdef MOD_ADC8_ON	
-	f_ConfigADC();
+#ifdef MOD_ADC8_ON
+    f_ConfigADC();
 #endif
 
 #ifdef MOD_DEBUGGER_ON
-	f_InitDebug();
+    f_InitDebug();
 #endif
 
-#ifdef MOD_FPGAXCS_ON	
-	f_CfgSPIFPGA();
-#endif
-	
-#ifdef MOD_LCMCHAR_ON	
-	f_ConfigLCM();
+#ifdef MOD_FPGAXCS_ON
+    f_CfgSPIFPGA();
 #endif
 
-#ifdef MOD_KEY44_ON	
-	f_ConfigKeyb4x4();
+#ifdef MOD_LCMCHAR_ON
+    f_ConfigLCM();
+#endif
+
+#ifdef MOD_KEY44_ON
+    f_ConfigKeyb4x4();
 #endif
 
 #ifdef MOD_LPC_ON
-	f_ConfigLPC();
+    f_ConfigLPC();
 #endif
 
 #ifdef MOD_PWMTIMER_ON
     f_InitPWM1();
-#endif 
+#endif
 
 #ifdef MOD_SCARD_ON
     f_Init_SCard();
-#endif 
+#endif
 
-#ifdef	MOD_SERAPP_ON
-	// Application Level Init
-	f_InitSerApp();
+#ifdef    MOD_SERAPP_ON
+    // Application Level Init
+    f_InitSerApp();
 #endif
 
 #ifdef DCFG_LOWPOW
-	cbi(MCUCR, SM0);                          // Idle mode for sleeps
-	cbi(MCUCR, SM1);
-	sbi(MCUCR, SE);
+    cbi(MCUCR, SM0);                          // Idle mode for sleeps
+    cbi(MCUCR, SM1);
+    sbi(MCUCR, SE);
 #endif
 
 
-	sei();                   /* enable interrupts */
-	f_Uart_ClrScr();
-	f_Uart_PutStr(c_VEROSSTR);
-	f_Uart_PutStr(c_VERAPPSTR);
+    sei();                   /* enable interrupts */
+    f_Uart_ClrScr();
+    f_Uart_PutStr(c_VEROSSTR);
+    f_Uart_PutStr(c_VERAPPSTR);
     f_Uart_PutStr("\nsalam slaam dastoor bedid\n");
 
 #ifdef MOD_LCMCHAR_ON
-	//f_LCM_WrMsgUnBuf(c_LCML1, c_VEROSSTR, c_FLASH);
-	//f_LCM_WrMsgF(c_LCML2, c_VERAPPSTR);
-	f_LCM_WrMsgUnBufF(c_LCML1, c_VEROSSTR);
-	f_LCM_WrMsgUnBufF(c_LCML2, c_VERAPPSTR);
-	f_LCM_WrMsgUnBufF(c_LCML3, "AVRILOS Demo");
-	f_LCM_WrMsgUnBufF(c_LCML4, "CodeProject!");
+    //f_LCM_WrMsgUnBuf(c_LCML1, c_VEROSSTR, c_FLASH);
+    //f_LCM_WrMsgF(c_LCML2, c_VERAPPSTR);
+    f_LCM_WrMsgUnBufF(c_LCML1, c_VEROSSTR);
+    f_LCM_WrMsgUnBufF(c_LCML2, c_VERAPPSTR);
+    f_LCM_WrMsgUnBufF(c_LCML3, "AVRILOS Demo");
+    f_LCM_WrMsgUnBufF(c_LCML4, "CodeProject!");
 #endif
 
 
+    for (;;) {
+        f_SysTick();
 
-    for(;;)
-    {
-			f_SysTick();
+#ifdef MOD_ADC8_ON
+        f_SysADC();
+#endif
 
-			#ifdef MOD_ADC8_ON
-			f_SysADC();
-			#endif
-			
-			#if defined (MOD_DEBUGGER_ON)
-			f_TaskDebugger();
-			#endif
+#if defined (MOD_DEBUGGER_ON)
+        f_TaskDebugger();
+#endif
 
-			#ifdef	MOD_SERAPP_ON
-			f_SerApp();
-			#endif
-			
-            #ifdef MOD_SCARD_ON
-			//f_SCard_CheckCardIn();
-            #endif
+#ifdef    MOD_SERAPP_ON
+        f_SerApp();
+#endif
 
-			#ifdef MOD_KEY44_ON			
+#ifdef MOD_SCARD_ON
+        //f_SCard_CheckCardIn();
+#endif
+
+#ifdef MOD_KEY44_ON
         f_Uart_PutStr("\ncode 166k\n");
-			if ( (v_key = f_GetKey()) != c_NOKEY) f_Uart_PutChar( v_key+'A');
-			#endif	
-			
-			#ifdef MOD_LCMCHAR_ON			
+            if ( (v_key = f_GetKey()) != c_NOKEY) f_Uart_PutChar( v_key+'A');
+#endif
+
+#ifdef MOD_LCMCHAR_ON
         f_Uart_PutStr("\ncode 171k\n");
-			//if ( (v_key = f_GetKey()) != c_NOKEY) f_LCM_PutChar(v_key+'A');
-			#endif	
-			
-			if ( ( v_SysStat & (1 << b_AppTick) ) != 0)
-			{
-                f_Uart_PutStr("\ncode 177k\n");
-				v_SysStat &= ~(1 << b_AppTick);
-				f_Applic();
-				
-			}
-			
-			#ifdef DCFG_LOWPOW
+            //if ( (v_key = f_GetKey()) != c_NOKEY) f_LCM_PutChar(v_key+'A');
+#endif
+
+        if ((v_SysStat & (1 << b_AppTick)) != 0) {
+            v_SysStat &= ~(1 << b_AppTick);
+            f_Applic();
+        }
+
+#ifdef DCFG_LOWPOW
         f_Uart_PutStr("\ncode 184k\n");
-			asm("sleep"::);
-			#endif
+            asm("sleep"::);
+#endif
 
     };              /* loop forever */
 }
