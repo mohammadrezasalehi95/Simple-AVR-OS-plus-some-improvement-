be4d2ef317eba342f2bed43b731ecf5008b4968f
mohammadreza salehi <mohammadreza@salamsalam.hopto.org>
2019-05-04 16:59:03 +0430

    compiled  [time: 16:59:03]

---
+++ avr_os/build.dep/debugger.d
@@ -0,0 +1,17 @@
+debugger.o: src/debug/debugger.c \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
+  c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/iom16.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h \
+  src/debug/../includes/types.h src/debug/../includes/settings.h \
+  src/debug/../includes/legacy.h src/debug/../periphint/Uart.h \
+  src/debug/../periphint/adc.h src/debug/../periphint/eeprom.h \
+  src/debug/../Utils/typeconv.h src/debug/debugger.h src/debug/dbgext.h
