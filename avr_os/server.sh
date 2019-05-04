f1955bdbafd1b5a11ac2894712f0e15633f68389
mohammadsaelhi95 <mohammadreza.salehi951@gmail.com>
2019-05-04 11:03:30 +0430

    km

--- avr_os/server.sh
+++ avr_os/server.sh
@@ -2,6 +2,11 @@ nc -k -l 4444 > watcher.out &
 while true; do
     inotifywait watcher.out
     git pull origin master
+    mkdir build.err
+    mkdir build.dep
+    mkdir build.lst
+    mkdir build.obj
+    mkdir build.rom
     echo salam
     echo make clean| wine cmd
     echo make | wine cmd
