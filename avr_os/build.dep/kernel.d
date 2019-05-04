91102ea145233fc7df095554eefc64755ef0e167
mohammadreza salehi <mohammadreza@salamsalam.hopto.org>
2019-05-04 10:59:54 +0430

    compiled  [time: 10:59:54]

---
+++ avr_os/build.dep/kernel.d
@@ -0,0 +1,24 @@
+kernel.o: src/applic/kernel.c \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/iom16.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h \
+  c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
+  src/applic/../includes/types.h src/applic/../includes/settings.h \
+  src/applic/../includes/legacy.h src/applic/../periphint/Uart.h \
+  src/applic/../periphint/Timer0.h \
+  src/applic/../periphint/../includes/ifc_time.h \
+  src/applic/../periphint/Timer1.h src/applic/../periphint/adc.h \
+  src/applic/../debug/debugger.h src/applic/../periphext/xcs_cfg.h \
+  src/applic/../Applic/serapp.h src/applic/../periphext/lcm_char.h \
+  src/applic/../periphext/keymat44.h src/applic/../periphext/lpc.h \
+  src/applic/../periphext/scard.h src/applic/../Applic/cfg/xcs05_cfg.h \
+  src/applic/../applic/serapp.h src/applic/../applic/applic.h
