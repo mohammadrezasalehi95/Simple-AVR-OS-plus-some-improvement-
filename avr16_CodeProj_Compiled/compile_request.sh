8676ca0e6e994baa2d313158b1896e6f8c7f9550
mohammadsaelhi95 <mohammadreza.salehi951@gmail.com>
2019-05-04 05:26:25 +0430

    bash compilit

---
+++ avr16_CodeProj_Compiled/compile_request.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+SERVER_IP=192.168.1.12
+SERVER_PORT=4444
+echo fire | telnet ${SERVER_IP} ${SERVER_PORT}
+git add .
+git commit -m $1
---
+++ avr16_CodeProj_Compiled/compiler_server.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+SERVER_PORT=4444
+nc -k -l ${SERVER_PORT} > watcher.out &
+trap ctrl_c INT
+function ctrl_c() {
+        rm watcher.out
+        echo exited
+        exit
+}
+while true; do
+    inotifywait watcher.out
+    git pull origin master
+    echo salam
+    echo make | wine cmd
+    now=$(date +"%T")
+    last_line=$(tail -1 watcher.out)
+    echo ${last_line}
+    git add .
+    git commit -m "compiled  [time: $now]"
+    git push origin master
+done
