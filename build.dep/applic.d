475b65c5e106edb780c6a3365f25e29f56cc2ba0
mohammadreza salehi <mohammadreza@salamsalam.hopto.org>
2019-05-05 08:49:23 +0430

    compiled  [time: 08:49:23]

--- build.dep/applic.d
+++ build.dep/applic.d
@@ -11,9 +11,10 @@ applic.o: src/applic/applic.c \
   c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
   c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h \
   c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
-  src/applic/../includes/types.h src/applic/../includes/settings.h \
-  src/applic/../includes/legacy.h src/applic/../Utils/delay.h \
-  src/applic/../periphint/Uart.h src/applic/../periphint/Timer0.h \
+  src/applic/../debug/debugger.h src/applic/../includes/types.h \
+  src/applic/../includes/settings.h src/applic/../includes/legacy.h \
+  src/applic/../Utils/delay.h src/applic/../periphint/Uart.h \
+  src/applic/../periphint/Timer0.h \
   src/applic/../periphint/../includes/ifc_time.h \
   src/applic/../periphint/Timer1.h src/applic/../periphext/scard.h \
   src/applic/applic.h
